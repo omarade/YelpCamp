4ec8c68ff9b9ca8319b69b86f5731c2d7ed2154e
Omar <3omar.a.de@gmail.com>
2017-09-30 20:53:32 +0000

    Creating new campgrounds
    Setup new campground POST route
    
    Setup form route

--- app.js
+++ app.js
@@ -8,17 +8,30 @@ app.set("views", __dirname + "/src/views");
 app.use("/", bodyParser());
 app.use(express.static("src/public"));
 
+let campgrounds = [
+    {name: "Salmon creek", image:"https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"},
+    {name: "Maas Mountain", image:"https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
+    {name: "Happy Hippies", image:"https://farm3.staticflickr.com/2655/3738566424_180036be3f.jpg"}
+];
+    
 app.get("/", (req, res) => {
     res.render("landing", {title: "Landing Page"});
 });
 
 app.get("/campgrounds", (req, res) => {
-    let campgrounds = [
-        {name: "Salmon creek", image:"https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"},
-        {name: "Maas Mountain", image:"https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
-        {name: "Happy Hippies", image:"https://farm3.staticflickr.com/2655/3738566424_180036be3f.jpg"}
-    ];
     res.render("campgrounds", {title: 'Campgrounds', campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", (req, res) => {
+    let name = req.body.name;
+    let image = req.body.image;
+    let newCamp = {name: name, image: image}
+    campgrounds.push(newCamp)
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("new", {title: "Add a Campground"});
 });
 
 const listener = app.listen(process.env.PORT, process.env.ID, () => {
